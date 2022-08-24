72c8d28130a0a3544044e21a96f47b2109c89567
AlexLan73 <diving_73@mail.ru>
2022-08-24 04:37:41 +0300

    GlobalUsing

---
+++ MLserver2.1/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using Convert.Logger;
+global using Convert.Moduls;
+global using Convert.Moduls.Config;
+global using Convert.Moduls.Error;
+global using MLServer_2._1;
+global using MLServer_2._1.Moduls;
+global using MLServer_2._1.Moduls.MDFRename;
